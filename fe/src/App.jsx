9b6f64061c88017104b912eabe00f09aa444f517
youngmin <dudals2963@tukrorea.ac.kr>
2025-05-08 21:56:43 +0900

    ✨ 이슈 뱃지 컴포넌트 구현 완료
    이슈의 활성화 여부에 따라 다른 svg파일을 렌더링하는 컴포넌트 구현 완료

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './styles/foundation';
 import GlobalStyle from './styles/GlobalStyle';
 import AppRouter from './routes/AppRouter';
-import IssueTitle from './base-ui/utils/IssueTitle';
 
 function App() {
   //추후에 zustand로 테마 상태 관리 예정
@@ -13,7 +12,6 @@ function App() {
     <ThemeProvider theme={IsDark ? darkTheme : lightTheme}>
       <GlobalStyle />
       {/* base-ui 컴포넌트 테스트 위치 */}
-
       {/* <AppRouter /> */}
     </ThemeProvider>
   );
