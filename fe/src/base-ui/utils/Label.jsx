3423a90f85b07494fa851dd313cd853c7a1d13b0
youngmin <dudals2963@tukrorea.ac.kr>
2025-05-09 12:36:43 +0900

    ✨ Label 유틸 base컴포넌트 구현 완료
    16진수 색상, 라벨타이틀을 입력받으면 입력받은Prop에 맞게 렌더링하는 라벨 컴포넌트   구현완료

--- fe/src/base-ui/utils/Label.jsx
+++ fe/src/base-ui/utils/Label.jsx
@@ -1,10 +1,24 @@
 /*
+- 라벨의 스타일 속성은 실게 깃헙의 라벨 스타일을 분석해 적용했습니다.
 - HEX 색상 코드 입력 시 색 변환해주는 로직 포함하는 base-ui
 - 입력: 16진수 색 코드, 라벨 텍스트
 - 출력: 입력받은 숫자 코드를 각 스타일로 적용한 라벨 렌더링
 */
 
 import styled from 'styled-components';
+import { typography } from '@/styles/foundation';
+import { radius } from '@/styles/foundation';
+import { getBorder } from '@/styles/foundation';
+
+const LabelWrapper = styled.div`
+  ${typography.display.medium12}
+  border-radius:${radius.large};
+  color: ${({ $textColor }) => $textColor};
+  background-color: ${({ $backgroundColor }) => $backgroundColor};
+  border: ${({ $borderColor }) => getBorder($borderColor).default};
+  height: 24px;
+  padding: 4px 12px;
+`;
 
 function generateLabelStyle(hexColor) {
   // HEX → RGB 변환
@@ -21,6 +35,16 @@ function generateLabelStyle(hexColor) {
   return { textColor, backgroundColor, borderColor };
 }
 
-export default function Label({ hexColor }) {
+export default function Label({ hexColor, labelTitle }) {
   const { textColor, backgroundColor, borderColor } = generateLabelStyle(hexColor);
+
+  return (
+    <LabelWrapper
+      $textColor={textColor}
+      $backgroundColor={backgroundColor}
+      $borderColor={borderColor}
+    >
+      {labelTitle}
+    </LabelWrapper>
+  );
 }
