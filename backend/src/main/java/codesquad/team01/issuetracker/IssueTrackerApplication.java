5023eb91724f32b5bf8551f902026b0e300dce28
jaehoonchoi <eeoos8840@gmail.com>
2025-05-09 12:56:28 +0900

    rename: 디렉토리 이름 수정

--- backend/src/main/java/codesquad/team01/issuetracker/IssueTrackerApplication.java
+++ backend/src/main/java/codesquad/team01/issuetracker/IssueTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.issue_tracker.backend;
+package codesquad.team01.issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/codesquad/team01/issuetracker/IssueTrackerApplicationTests.java
+++ backend/src/test/java/codesquad/team01/issuetracker/IssueTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.issue_tracker.backend;
+package codesquad.team01.issuetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
