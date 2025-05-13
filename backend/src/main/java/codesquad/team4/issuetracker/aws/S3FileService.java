cd6584b5fcf47f23c3faec1e9e4c5f6c6a4961da
subin <eltm521@naver.com>
2025-05-13 15:20:34 +0900

    feat : S3 파일 업로드 기능 구현 - aws 의존성 추가 - S3 설정 및 로그 설정 추가 - 파일 업로드 기능 구현

---
+++ backend/src/main/java/codesquad/team4/issuetracker/aws/S3FileService.java
@@ -0,0 +1,58 @@
+package codesquad.team4.issuetracker.aws;
+
+import codesquad.team4.issuetracker.exception.FileUploadException;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class S3FileService {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile file, String directory) {
+        return Optional.ofNullable(file)
+                .filter(f -> !f.isEmpty())  // 파일이 비어있지 않은 경우에만 처리
+                .map(f -> {
+                    String originalFilename = f.getOriginalFilename();
+                    String key = directory + UUID.randomUUID() + "_" + originalFilename;
+
+                    // S3에 파일을 업로드하기 위한 요청 객체 생성
+                    PutObjectRequest putObjectRequest = getRequest(f, key);
+
+                    // S3 클라이언트를 통해 파일 업로드
+                    try {
+                        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(f.getInputStream(), f.getSize()));
+                    } catch (IOException e) {
+                        log.error("파일 업로드 중 오류 발생: {}", e.getMessage(), e);
+                        throw new FileUploadException("파일 업로드에 실패했습니다.", e);
+                    }
+
+                    log.info("S3 업로드 성공");
+                    return String.format("https://%s.s3.amazonaws.com/%s", bucket, key);
+                })
+                .orElse("");  // 파일이 null이거나 비어있으면 빈 문자열 반환
+    }
+
+    private PutObjectRequest getRequest(MultipartFile f, String key) {
+        return PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .acl("public-read") // 퍼블릭 읽기 권한
+                .contentType(f.getContentType())
+                .build();
+    }
+}
