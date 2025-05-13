603c83317333fe5ae401038ec57cd4ddab478527
HeeSung Choi <gladhee@icloud.com>
2025-05-14 00:03:56 +0900

    feat(BaseEntity): introduce BaseEntity for auditing fields

---
+++ backend/src/main/java/elbin_bank/issue_tracker/common/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package elbin_bank.issue_tracker.common.domain;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
+
+import java.time.LocalDateTime;
+
+@Getter
+public abstract class BaseEntity {
+
+    @Column("created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column("updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column("deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void markDeleted(LocalDateTime when) {
+        this.deletedAt = when;
+    }
+
+}
