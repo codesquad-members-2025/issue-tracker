f8578eec057da0f28bcd7e6b553852be084e1e1f
jang-jinuk <dlsndifn98@gmail.com>
2025-05-13 15:15:12 +0900

    ✅ User 엔티티 생성
    - loginId, password, email, userName 포함

---
+++ be/src/main/java/CodeSquad/IssueTracker/user/User.java
@@ -0,0 +1,19 @@
+package CodeSquad.IssueTracker.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Builder
+@Table("users")
+public class User {
+    @Id
+    private Long id;
+    private String loginId;
+    private String password;
+    private String email;
+    private String userName;
+}
+
