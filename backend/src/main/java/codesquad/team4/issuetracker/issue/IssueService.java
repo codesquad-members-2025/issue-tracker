47f6e703152674655e4ef85c3f7e4d9b76015539
subin <eltm521@naver.com>
2025-05-13 12:06:58 +0900

    feat : 이슈 생성 기능 구현 - RequestDto로 이슈 생성 -> 이슈 라벨, 담당자 생성 - @Transactional로 데이터 무결성 보장

--- backend/src/main/java/codesquad/team4/issuetracker/issue/IssueService.java
+++ backend/src/main/java/codesquad/team4/issuetracker/issue/IssueService.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import codesquad.team4.issuetracker.label.dto.LabelDto;
 import codesquad.team4.issuetracker.user.dto.UserDto;
