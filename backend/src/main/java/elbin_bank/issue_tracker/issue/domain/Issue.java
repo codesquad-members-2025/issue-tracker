548429fba9e332715efd48151af706d521e6e1da
HeeSung Choi <gladhee@icloud.com>
2025-05-14 00:04:28 +0900

    feat(issue): implement issue entity

--- backend/src/main/java/elbin_bank/issue_tracker/issue/domain/Issue.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/domain/Issue.java
@@ -4,23 +4,34 @@ import elbin_bank.issue_tracker.common.domain.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("issue")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 public class Issue extends BaseEntity {
 
     @Id
-    private long id;
-    private long authorId;
-    private long milestoneId;
+    private Long id;
+
+    @Column("writer_id")
+    private Long authorId;
+
+    @Column("milestone_id")
+    private Long milestoneId;
+
     private String title;
+
+    @Column("contents")
     private String contents;
-    private boolean isClosed;
+
+    @Column("is_closed")
+    private Boolean isClosed;
+
+    @Column("file_path")
+    private String filePath;
 
 }
