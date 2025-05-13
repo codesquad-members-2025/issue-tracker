e64e27c2dd2af937508c4d2d3c1ce5ba4f12d34c
HeeSung Choi <gladhee@icloud.com>
2025-05-14 00:43:45 +0900

    feat(IssueRepository): remove CrudRepository inheritance

--- backend/src/main/java/elbin_bank/issue_tracker/issue/domain/IssueRepository.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/domain/IssueRepository.java
@@ -1,10 +1,8 @@
 package elbin_bank.issue_tracker.issue.domain;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
-public interface IssueRepository extends CrudRepository<Issue, Long> {
+public interface IssueRepository {
 
     List<Issue> findByFilter(Boolean isClosed);
 
