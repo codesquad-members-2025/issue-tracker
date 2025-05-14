6c5e868a659663f17a092c645dc313a96a69f0b5
HeeSung Choi <gladhee@icloud.com>
2025-05-14 16:19:11 +0900

    feat(IssueQueryController): add api endpoint

--- backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/IssueQueryController.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/IssueQueryController.java
@@ -2,7 +2,6 @@ package elbin_bank.issue_tracker.issue.presentation;
 
 import elbin_bank.issue_tracker.issue.application.query.GetFilteredIssueListService;
 import elbin_bank.issue_tracker.issue.application.query.dto.IssuesResponseDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +17,8 @@ public class IssueQueryController {
     }
 
     @GetMapping("")
-    public ResponseEntity<IssuesResponseDto> list(@RequestParam(value = "q", required = false) String q) {
-        try {
-            return ResponseEntity.ok(getFilteredIssueListService.find(q));
-        } catch (IllegalStateException e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public IssuesResponseDto list(@RequestParam(value = "q", required = false) String q) {
+        return getFilteredIssueListService.find(q);
     }
 
 }
