e2abbef8908438fc43049fa44f3d28994e46dedf
jang-jinuk <dlsndifn98@gmail.com>
2025-05-14 12:51:02 +0900

    ✅ 토큰 생성 로직 분리 및 refreshToken 저장
    - 토큰 생성 로직을 메서드로 분리
    - DB에 refreshToken 저장

--- be/src/main/java/CodeSquad/IssueTracker/login/service/LoginService.java
+++ be/src/main/java/CodeSquad/IssueTracker/login/service/LoginService.java
@@ -4,12 +4,15 @@ import CodeSquad.IssueTracker.login.dto.LoginRequestDto;
 import CodeSquad.IssueTracker.login.dto.LoginResponseDto;
 import CodeSquad.IssueTracker.login.exception.PasswordMismatchException;
 import CodeSquad.IssueTracker.login.exception.UserNotFoundException;
-import CodeSquad.IssueTracker.user.repository.JdbcTemplatesUserRepository;
-import CodeSquad.IssueTracker.user.JWTUtil;
+import CodeSquad.IssueTracker.token.RefreshToken;
+import CodeSquad.IssueTracker.token.repository.RefreshTokenRepository;
+import CodeSquad.IssueTracker.token.util.JWTUtil;
 import CodeSquad.IssueTracker.user.User;
+import CodeSquad.IssueTracker.user.repository.JdbcTemplatesUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -19,29 +22,40 @@ import java.util.Optional;
 public class LoginService {
 
     private final JdbcTemplatesUserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final JWTUtil jwtUtil;
 
     /*
     사용자가 입력한 loginId, password와 대조하여 일치하면 Access Token과 Refresh Token을 반환하고 아니면 예외를 발생시킵니다. Refresh Token은 DB에도 저장합니다.
      */
+    @Transactional
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
         User targetUser = getUser(loginRequestDto.getLoginId());
         validatePassword(targetUser, loginRequestDto.getPassword());
-
-        String accessToken = jwtUtil.createAccessToken(targetUser, "");
-        String refreshToken = jwtUtil.createRefreshToken();
-
-        return new LoginResponseDto(accessToken, refreshToken);
+        return generateToken(targetUser, "프로필 사진 URL");
     }
 
-    public User getUser(String loginId) {
+    private User getUser(String loginId) {
         Optional<User> foundUser = userRepository.findUserByLoginId(loginId);
         return foundUser.orElseThrow(UserNotFoundException::new);
     }
 
-    public void validatePassword(User targetUser, String password) {
+    private void validatePassword(User targetUser, String password) {
         if (!targetUser.getPassword().equals(password)) {
             throw new PasswordMismatchException();
         }
+    }
+
+    private LoginResponseDto generateToken(User targetUser, String imgUrl) {
+        String accessToken = jwtUtil.createAccessToken(targetUser, imgUrl);
+        String refreshToken = jwtUtil.createRefreshToken();
+
+        refreshTokenRepository.save(RefreshToken.builder()
+                .loginId(targetUser.getLoginId())
+                .refreshToken(refreshToken)
+                .expiration(System.currentTimeMillis() + JWTUtil.REFRESH_EXPIRATION_TIME)
+                .build());
+
+        return new LoginResponseDto(accessToken, refreshToken);
     }
 }
