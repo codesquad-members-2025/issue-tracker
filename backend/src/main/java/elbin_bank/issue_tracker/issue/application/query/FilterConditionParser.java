91f7627d73e5badf8a44de859bd22eff6cc20c73
HeeSung Choi <gladhee@icloud.com>
2025-05-14 18:18:40 +0900

    fix(FilterConditionParser): filter bug fix
    - change isClosed default value to false at filter conditon parser

--- backend/src/main/java/elbin_bank/issue_tracker/issue/application/query/FilterConditionParser.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/application/query/FilterConditionParser.java
@@ -3,22 +3,29 @@ package elbin_bank.issue_tracker.issue.application.query;
 public class FilterConditionParser {
 
     public static FilterCriteria parse(String q) {
-        Boolean isClosed = null;
+        boolean isClosed = false;
+        boolean open = false;
+        boolean closed = false;
 
         if (q != null && !q.isBlank()) {
             String[] parts = q.split(" ");
 
             for (String p : parts) {
                 if (p.startsWith("state:")) {
-                    boolean open = p.equals("state:open");
-                    boolean close = p.equals("state:closed");
-                    if (open && close) {
-                        throw new IllegalStateException("Filter conditions cannot be open or open closed");
+                    if (p.equals("state:closed")) {
+                        closed = true;
                     }
-                    isClosed = close;
+                    if (p.equals("state:open")) {
+                        open = true;
+                    }
                 }
             }
+            if (open && closed) {
+                throw new IllegalStateException("Filter conditions cannot be open or open closed");
+            }
+            isClosed = closed;
         }
+
         return new FilterCriteria(isClosed);
     }
 
