4ddfba2a12dc986c39163f84aab074975ebe5d61
jang-jinuk <dlsndifn98@gmail.com>
2025-05-14 16:34:34 +0900

    ♻️ LoginController에 BaseResponseDto 제네릭 적용 및 명확한 응답 구조 반영
    - DefaultDto → BaseResponseDto로 변경
    - 불필요한 ResponseEntity 제거
    - 응답 구조를 제네릭을 통해 명확히 표현

--- be/src/main/java/CodeSquad/IssueTracker/login/controller/LoginController.java
+++ be/src/main/java/CodeSquad/IssueTracker/login/controller/LoginController.java
@@ -1,12 +1,11 @@
 package CodeSquad.IssueTracker.login.controller;
 
-import CodeSquad.IssueTracker.global.dto.DefaultDto;
+import CodeSquad.IssueTracker.global.dto.BaseResponseDto;
 import CodeSquad.IssueTracker.login.service.LoginService;
 import CodeSquad.IssueTracker.login.dto.LoginRequestDto;
 import CodeSquad.IssueTracker.login.dto.LoginResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +17,9 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<DefaultDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
+    public BaseResponseDto<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
         LoginResponseDto response = loginService.login(loginRequestDto);
-        return ResponseEntity.ok(new DefaultDto(true, "로그인을 성공했습니다.", response));
+        return BaseResponseDto.success("로그인을 성공했습니다.", response);
     }
 
 }
