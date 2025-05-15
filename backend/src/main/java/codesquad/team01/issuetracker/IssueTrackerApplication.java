b2ad0849e658809dbadc8344df60f7c8f45df705
doopang24 <wodus4145@naver.com>
2025-05-15 21:13:32 +0900

    feat: github oauth 설정 정보 매핑 및 빈 등록

--- backend/src/main/java/codesquad/team01/issuetracker/IssueTrackerApplication.java
+++ backend/src/main/java/codesquad/team01/issuetracker/IssueTrackerApplication.java
@@ -1,8 +1,11 @@
 package codesquad.team01.issuetracker;
 
+import codesquad.team01.issuetracker.common.config.GithubOAuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(GithubOAuthProperties.class)
 @SpringBootApplication
 public class IssueTrackerApplication {
 
