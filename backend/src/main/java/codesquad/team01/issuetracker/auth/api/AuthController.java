569afb38e2683fcef6631289a7b21e2dbdd67d34
doopang24 <wodus4145@naver.com>
2025-05-15 21:39:26 +0900

    feat: 깃헙으로 로그인 요청 시, GitHub authorization url로 302 리다이렉트 처리

--- backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
@@ -1,4 +1,24 @@
 package codesquad.team01.issuetracker.auth.api;
 
+import codesquad.team01.issuetracker.auth.util.AuthorizationUrlBuilder;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthorizationUrlBuilder authorizationUrlBuilder;
+
+    @GetMapping("/api/v1/oauth/github/login")
+    public void redirectToGithub(HttpServletResponse response, HttpSession session) throws IOException {
+        URI githubUri = authorizationUrlBuilder.buildAuthorizeUri(session);
+        response.sendRedirect(githubUri.toString());
+    }
 }
