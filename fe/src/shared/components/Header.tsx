8ecd1115efd134c25a85227b51489153755a905d
wan <conut22@naver.com>
2025-05-15 17:22:10 +0900

    feat(shared): implement header with sample profile
    created a shared Header component with a logo on the left and a sample profile image on the right.
    currently using a static image (`/images/sampleProfile.png`).
    fetching actual user data and rendering the profile dynamically will be implemented later.

---
+++ fe/src/shared/components/Header.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+import Logo from '@/assets/logoMedium.svg?react';
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <LeftSection>
+        <StyledLogo />
+      </LeftSection>
+      <Profile profileImageUrl="/images/sampleProfile.png" />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 27px 0;
+`;
+
+const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const StyledLogo = styled(Logo)`
+  width: 199px;
+  height: 40px;
+  color: ${({ theme }) => theme.textColor.strong};
+`;
+
+// TODO profile 공통 컴포넌트 분리
+interface Props {
+  profileImageUrl: string;
+}
+
+const Profile = ({ profileImageUrl }: Props) => {
+  return <Avatar src={profileImageUrl} alt="user-profile" />;
+};
+
+const Avatar = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 1px solid white;
+  object-fit: cover;
+`;
