4b8f40c9eb6fd99d4978c84686c6a63f76f0beff
HeeSung Choi <gladhee@icloud.com>
2025-05-16 22:22:11 +0900

    feat(GetFilteredIssueListService): integrate UserRepository for author nickname retrieval

--- backend/src/main/java/elbin_bank/issue_tracker/issue/application/query/GetFilteredIssueListService.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/application/query/GetFilteredIssueListService.java
@@ -7,30 +7,24 @@ import elbin_bank.issue_tracker.issue.domain.IssueRepository;
 import elbin_bank.issue_tracker.issue.domain.IssueStatus;
 import elbin_bank.issue_tracker.label.application.query.dto.LabelsDto;
 import elbin_bank.issue_tracker.label.domain.Label;
+import elbin_bank.issue_tracker.user.domain.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
-import static java.util.Collections.emptyList;
 
 @Service
 public class GetFilteredIssueListService {
 
     private final IssueRepository issueRepository;
+    private final UserRepository userRepository;
 
-    public GetFilteredIssueListService(IssueRepository issueRepository) {
+    public GetFilteredIssueListService(IssueRepository issueRepository, UserRepository userRepository) {
         this.issueRepository = issueRepository;
+        this.userRepository = userRepository;
     }
-
-    private static final Map<Long, String> userNames = Map.of(
-            0L, "elbin",
-            1L, "glad",
-            2L, "wanja",
-            3L, "wanja",
-            4L, "gd"
-    );
 
     @Transactional(readOnly = true)
     public IssuesResponseDto find(String q) {
@@ -43,25 +37,24 @@ public class GetFilteredIssueListService {
         List<Long> issueIds = issues.stream().map(Issue::getId).toList();
         List<Long> authorIds = issues.stream().map(Issue::getAuthorId).distinct().toList();
 
-//        Map<Long, String> authors = userRepo.findNicknamesByIds(authorIds); @todo
+        Map<Long, String> authors = userRepository.findNickNamesByIds(authorIds);
         Map<Long, List<Label>> labelsMap = issueRepository.findLabelsByIssueIds(issueIds);
         Map<Long, List<String>> assigneesMap = issueRepository.findAssigneesByIssueIds(issueIds);
 
         List<IssueSummaryDto> dtos = issues.stream().map(i ->
-                        new IssueSummaryDto(
-                                i.getId(),
-//                        authors.get(i.getAuthorId()), @todo
-                                userNames.get(i.getAuthorId()),
-                                i.getTitle(),
-                                labelsMap.getOrDefault(i.getId(), emptyList())
-                                        .stream().map(label -> new LabelsDto(label.getId(), label.getName(), label.getDescription(), label.getColor())
-                                        ).toList(),
-                                i.isClosed(),
-                                i.getCreatedAt(),
-                                i.getUpdatedAt(),
-                                assigneesMap.getOrDefault(i.getId(), emptyList()),
-                                "hello wanja" // @todo
-                        )
+                new IssueSummaryDto(
+                        i.getId(),
+                        authors.get(i.getAuthorId()),
+                        i.getTitle(),
+                        labelsMap.getOrDefault(i.getId(), Collections.emptyList())
+                                .stream().map(label -> new LabelsDto(label.getId(), label.getName(), label.getDescription(), label.getColor())
+                                ).toList(),
+                        i.isClosed(),
+                        i.getCreatedAt(),
+                        i.getUpdatedAt(),
+                        assigneesMap.getOrDefault(i.getId(), Collections.emptyList()),
+                        "hello wanja" // @todo
+                )
         ).toList();
 
         long openCount = issueRepository.countByStatus(IssueStatus.OPEN);
