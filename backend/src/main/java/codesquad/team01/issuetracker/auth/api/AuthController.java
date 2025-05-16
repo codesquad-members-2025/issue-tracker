5912d5e69d3a6968d9938fb9c59b2ed4ed295118
doopang24 <wodus4145@naver.com>
2025-05-16 11:40:25 +0900

    feat: 인증 코드 수신 로직 구현
    인증 서버에서 리다이렉트 uri로 온 authorization code, state 수신하는 메서드 구현

--- backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
@@ -1,10 +1,13 @@
 package codesquad.team01.issuetracker.auth.api;
 
+import codesquad.team01.issuetracker.auth.dto.LoginResponse;
+import codesquad.team01.issuetracker.auth.service.AuthService;
 import codesquad.team01.issuetracker.auth.util.AuthorizationUrlBuilder;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -14,11 +17,26 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
     private final AuthorizationUrlBuilder authorizationUrlBuilder;
 
     @GetMapping("/api/v1/oauth/github/login")
     public void redirectToGithub(HttpServletResponse response, HttpSession session) throws IOException {
         URI githubUri = authorizationUrlBuilder.buildAuthorizeUri(session);
         response.sendRedirect(githubUri.toString());
+    }
+
+    @GetMapping("/api/v1/oauth/callback")
+    public LoginResponse githubCallback(
+            @RequestParam("code") String code,
+            @RequestParam("state") String state,
+            HttpSession session) {
+        String savedState = (String) session.getAttribute("oauth_state");
+        if (savedState == null || !savedState.equals(state)) {
+            throw new IllegalStateException("유효하지 않은 state");
+        }
+        session.removeAttribute("oauth_state");
+
+        return authService.loginWithGitHub(code);
     }
 }
