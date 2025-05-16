bc5acf3b4c858739f011dcfce54332df76276acb
doopang24 <wodus4145@naver.com>
2025-05-16 20:13:13 +0900

    comment: 주석 및 상수 추가

--- backend/src/main/java/codesquad/team01/issuetracker/common/config/GithubOAuthProperties.java
+++ backend/src/main/java/codesquad/team01/issuetracker/common/config/GithubOAuthProperties.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-// application.yaml 의 github.oauth 아래에 정의된 모든 프로퍼티를 이 클래스의 필드와 자동으로 매핑해준다
+// application.yaml 의 github.oauth 아래에 정의된 모든 프로퍼티를 이 클래스의 필드와 자동으로 매핑
 @Data
 @NoArgsConstructor
 @ConfigurationProperties(prefix = "github.oauth")
