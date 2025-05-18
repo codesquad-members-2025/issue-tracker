4c2741e0d63028cb00bc16cb858be63abb0b63a0
subin <eltm521@naver.com>
2025-05-18 15:41:28 +0900

    chore : S3 접근 방식 IAM ROLE로 변경

--- backend/src/main/java/codesquad/team4/issuetracker/aws/S3Config.java
+++ backend/src/main/java/codesquad/team4/issuetracker/aws/S3Config.java
@@ -14,18 +14,11 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
