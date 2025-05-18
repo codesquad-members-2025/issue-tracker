c69bff1abad58703e921c55a0e9731c671f0aede
wan <conut22@naver.com>
2025-05-18 22:42:28 +0900

    fix(core): resolve API request failure in production
    the issue was caused by hardcoded localhost in env variables during build,
    which led to API calls targeting localhost even in production
    replaced the API path with a relative path (`/api/v1`) to resolve this
    vite proxy is used only in development, so localhost is bypassed
    also removed .env files from Git tracking since they are no longer needed

--- fe/src/shared/constants/api.ts
+++ fe/src/shared/constants/api.ts
@@ -1,7 +1,6 @@
 const API_VERSION = '/api/v1';
-const API_HOST = import.meta.env.VITE_API_HOST; // 환경변수로 도메인 관리
 
-export const API_BASE_URL = `${API_HOST}${API_VERSION}`;
+export const API_BASE_URL = `${API_VERSION}`;
 
 export const API = {
   ISSUES: `${API_BASE_URL}/issues`,
--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,25 +1,21 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react(), svgr()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
       },
     },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_HOST,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+  },
 });
