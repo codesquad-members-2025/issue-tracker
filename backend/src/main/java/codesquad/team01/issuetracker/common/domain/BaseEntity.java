faa34147d46a06b3f544a6a63d75cfd7ea8a0d41
jaehoonchoi <eeoos8840@gmail.com>
2025-05-19 21:44:53 +0900

    refactor(domain): BaseEntity 상속구조 적용
    - Issue 엔티티에 BaseEntity 상속 적용
    - Issue 엔티티에서 createdAt, updatedAt 필드 제거
    - 빌더 패턴 생성자 방식으로 수정

---
+++ backend/src/main/java/codesquad/team01/issuetracker/common/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package codesquad.team01.issuetracker.common.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public boolean isDeleted() {
+        return deletedAt != null;
+    }
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+    }
+}
--- backend/src/main/java/codesquad/team01/issuetracker/issue/domain/Issue.java
+++ backend/src/main/java/codesquad/team01/issuetracker/issue/domain/Issue.java
@@ -1,5 +1,6 @@
 package codesquad.team01.issuetracker.issue.domain;
 
+import codesquad.team01.issuetracker.common.domain.BaseEntity;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
@@ -7,11 +8,9 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table("Issue")
-public class Issue {
+public class Issue extends BaseEntity {
 
     @Id
     private Long id;
@@ -19,18 +18,23 @@ public class Issue {
     private String title;
     private String content;
 
-    @Builder.Default
     private boolean isOpen = true;
-
-    @Builder.Default
-    private LocalDateTime createdAt = LocalDateTime.now();
-
-    @Builder.Default
-    private LocalDateTime updatedAt = LocalDateTime.now();
 
     private LocalDateTime closedAt;
 
     private Long writerId;
 
     private Long milestoneId;
+
+    @Builder
+    private Issue(Long id, String title, String content, boolean isOpen,
+                  LocalDateTime closedAt, Long writerId, Long milestoneId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.isOpen = isOpen;
+        this.closedAt = closedAt;
+        this.writerId = writerId;
+        this.milestoneId = milestoneId;
+    }
 }
