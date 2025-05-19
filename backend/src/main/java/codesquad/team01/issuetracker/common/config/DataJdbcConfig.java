e9421a0e2d1aafc59e10fa629b86c9af96532e36
doopang24 <wodus4145@naver.com>
2025-05-20 08:29:13 +0900

    feat: 컬럼 이름 전역 설정

--- backend/src/main/java/codesquad/team01/issuetracker/common/config/DataJdbcConfig.java
+++ backend/src/main/java/codesquad/team01/issuetracker/common/config/DataJdbcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.data.relational.core.mapping.NamingStrategy;
+import org.springframework.data.relational.core.mapping.RelationalPersistentProperty;
 
 import java.util.Optional;
 
@@ -14,5 +16,30 @@ public class DataJdbcConfig {
     @Bean
     public AuditorAware<String> auditorProvider() {
         return () -> Optional.of("system");
+    }
+
+    @Bean
+    public NamingStrategy namingStrategy() {
+        return new NamingStrategy() {
+            @Override
+            public String getTableName(Class<?> type) {
+                return toSnake(type.getSimpleName());
+            }
+
+            @Override
+            public String getSchema() {
+                return "";
+            }
+
+            @Override
+            public String getColumnName(RelationalPersistentProperty property) {
+                return toSnake(property.getName());
+            }
+
+            private String toSnake(String input) {
+                return input.replaceAll("([a-z])([A-Z])", "$1_$2")
+                        .toLowerCase();
+            }
+        };
     }
 }
