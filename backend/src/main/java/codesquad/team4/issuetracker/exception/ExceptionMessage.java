2a7456b8e98d8d0d926f008152db19ce3b8dd21c
subin <eltm521@naver.com>
2025-05-20 12:31:54 +0900

    feat : ControllerAdvice 커스템 에러 등록 - MilestoneNotFoundException - FileUploadException

--- backend/src/main/java/codesquad/team4/issuetracker/exception/ExceptionMessage.java
+++ backend/src/main/java/codesquad/team4/issuetracker/exception/ExceptionMessage.java
@@ -6,4 +6,5 @@ public class ExceptionMessage {
     public static final String FILE_UPLOAD_FAILED = "파일 업로드에 실패했습니다.";
     public static final String UPDATE_ISSUE_FAILED =  "이슈가 존재하지 않습니다";
     public static final String NOT_FOUND_ISSUE = "이슈를 찾을 수 없습니다. issueId = ";
+    public static final String NOT_FOUND_MILESTONE = "마일스톤을 찾을 수 없습니다. milestoneId = ";
 }
--- backend/src/main/java/codesquad/team4/issuetracker/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/codesquad/team4/issuetracker/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package codesquad.team4.issuetracker.exception;
 
+import static codesquad.team4.issuetracker.exception.ExceptionMessage.FILE_UPLOAD_FAILED;
 import static codesquad.team4.issuetracker.exception.ExceptionMessage.NOT_FOUND_ISSUE;
+import static codesquad.team4.issuetracker.exception.ExceptionMessage.NOT_FOUND_MILESTONE;
 
+import codesquad.team4.issuetracker.issue.dto.IssueResponseDto;
 import codesquad.team4.issuetracker.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,4 +20,15 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.fail(NOT_FOUND_ISSUE + ex.getIssueId()));
     }
 
+    @ExceptionHandler(MilestoneNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleMilestoneNotFoundException(MilestoneNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.fail(NOT_FOUND_MILESTONE + ex.getMilestoneId()));
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public ResponseEntity<ApiResponse<?>> handleFileUploadException(FileUploadException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.fail(FILE_UPLOAD_FAILED));
+    }
 }
