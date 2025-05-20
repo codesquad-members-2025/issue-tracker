95696dc292cf868103fc3f3af1ae6fec59c1dfd3
2jiyong <jdragon2000@naver.com>
2025-05-20 17:17:24 +0900

    feat: 쿼리스트링 파서 추가

--- backend/src/main/java/com/team5/issue_tracker/issue/dto/IssueSearchCondition.java
+++ backend/src/main/java/com/team5/issue_tracker/issue/dto/IssueSearchCondition.java
@@ -5,8 +5,10 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter //TODO: Setter를 나중에 빌더 패턴으로 변경할 것
 @NoArgsConstructor
 @AllArgsConstructor
 public class IssueSearchCondition {
