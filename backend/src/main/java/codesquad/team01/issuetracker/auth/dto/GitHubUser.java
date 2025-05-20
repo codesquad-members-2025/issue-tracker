ea6d3713ef82a203d5f470c1ea16114ae61afdd9
doopang24 <wodus4145@naver.com>
2025-05-20 11:26:53 +0900

    refactor: 깃헙에서 사용자의 이메일을 가져올 수 있도록 하는 로직 구현

--- backend/src/main/java/codesquad/team01/issuetracker/auth/dto/GitHubUser.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/dto/GitHubUser.java
@@ -1,8 +1,10 @@
 package codesquad.team01.issuetracker.auth.dto;
 
+// 깃헙이 주는 사용자의 정보를 담는 dto
 public record GitHubUser(
-        Long id,
+        long id,
         String githubId,
-        String avatarUrl
+        String avatarUrl,
+        String email
 ) {
 }
