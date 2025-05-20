39e26fb43b4634d8a398efca0f85a876af20e679
이상윤 <sylagape1231@gmail.com>
2025-05-20 11:57:15 +0900

    feat: 로고 클릭 시 새로고침 기능 추가

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,22 +1,34 @@
 import Image from "next/image";
 import { useThemeStore } from "@lib/useThemeStore";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  cursor: pointer;
+`;
 
 export default function Logo() {
   const isDarkMode = useThemeStore((state) => state.isDark);
+  const handleClickRefresh = () => {
+    window.location.reload();
+  };
 
-  return isDarkMode ? (
-    <Image
-      src="/icons/logoIconDark.svg"
-      alt="이슈 트래커 로고 - 다크모드"
-      width={199}
-      height={40}
-    />
-  ) : (
-    <Image
-      src="/icons/logoIcon.svg"
-      alt="이슈 트래커 로고"
-      width={199}
-      height={40}
-    />
+  return (
+    <Container onClick={handleClickRefresh}>
+      {isDarkMode ? (
+        <Image
+          src="/icons/logoIconDark.svg"
+          alt="이슈 트래커 로고 - 다크모드"
+          width={199}
+          height={40}
+        />
+      ) : (
+        <Image
+          src="/icons/logoIcon.svg"
+          alt="이슈 트래커 로고"
+          width={199}
+          height={40}
+        />
+      )}
+    </Container>
   );
 }
