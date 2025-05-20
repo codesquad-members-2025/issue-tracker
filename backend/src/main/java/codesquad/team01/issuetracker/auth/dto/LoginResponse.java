11c3557edb862fb2481976d7b2977af089aa8860
doopang24 <wodus4145@naver.com>
2025-05-20 16:19:09 +0900

    feat: providerId로 User 조회하는 로직 구현

--- backend/src/main/java/codesquad/team01/issuetracker/auth/dto/LoginResponse.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/dto/LoginResponse.java
@@ -3,8 +3,7 @@ package codesquad.team01.issuetracker.auth.dto;
 // 서비스 관점에서 로그인에 필요한 정보를 담은 dto
 // jwt, id, 프로필 정보 등
 public record LoginResponse(
-        String id,
-        String avatarUrl,
+        int id,
         String email
 ) {
 }
