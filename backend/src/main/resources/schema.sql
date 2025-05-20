5643f9aef5628f431bb3325c074186ab1f696c41
2jiyong <jdragon2000@naver.com>
2025-05-20 15:05:33 +0900

    refactor: Unique 추가 및 comment의 user에 not null 삭제

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE milestone
 (
     id          BIGINT PRIMARY KEY AUTO_INCREMENT,
     deadline    DATETIME     NOT NULL,
-    name        VARCHAR(255) NOT NULL,
+    name        VARCHAR(255) NOT NULL UNIQUE,
     description TEXT,
     is_open     BOOLEAN      NOT NULL,
     created_at  DATETIME     NOT NULL,
@@ -11,8 +11,8 @@ CREATE TABLE milestone
 CREATE TABLE user
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(255) NOT NULL,
-    email      VARCHAR(255) NOT NULL,
+    username   VARCHAR(255) NOT NULL UNIQUE,
+    email      VARCHAR(255) NOT NULL UNIQUE,
     image_url  VARCHAR(255),
     password   VARCHAR(255) NOT NULL,
     created_at DATETIME     NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE issue
 CREATE TABLE comment
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    user_id    BIGINT   NOT NULL,
+    user_id    BIGINT,
     issue_id   BIGINT   NOT NULL,
     content    TEXT     NOT NULL,
     created_at DATETIME NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE comment
 CREATE TABLE label
 (
     id               BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name             VARCHAR(255) NOT NULL,
+    name             VARCHAR(255) NOT NULL UNIQUE,
     description      TEXT,
     text_color       VARCHAR(50)  NOT NULL,
     background_color VARCHAR(50)  NOT NULL,
