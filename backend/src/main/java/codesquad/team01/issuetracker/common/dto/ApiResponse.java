df1917beacbf4f8515ce5444aaa24f9a597294ca
jaehoonchoi <eeoos8840@gmail.com>
2025-05-20 19:17:04 +0900

    feat(common-dto): ApiResponse 클래스에 성공 여부를 정의하는 success 필드 추가

--- backend/src/main/java/codesquad/team01/issuetracker/common/dto/ApiResponse.java
+++ backend/src/main/java/codesquad/team01/issuetracker/common/dto/ApiResponse.java
@@ -8,17 +8,29 @@ import lombok.Getter;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
+
+    private final boolean success;
     private final T data;
     private final String message;
 
     public static <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
+                .success(true)
                 .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) { // 성공해도 메세지가 필요할 경우 - 쓸 일이 있을까?
+        return ApiResponse.<T>builder()
+                .success(true)
+                .data(data)
+                .message(message)
                 .build();
     }
 
     public static ApiResponse<?> error(String message) {
         return ApiResponse.builder()
+                .success(false)
                 .message(message)
                 .build();
     }
