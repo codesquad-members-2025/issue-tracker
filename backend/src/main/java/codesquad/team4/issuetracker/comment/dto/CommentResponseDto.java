b0ac6285b3b164b99a1ddb6f6dc6ed19ace932bb
subin <eltm521@naver.com>
2025-05-21 12:08:32 +0900

    chore : EOF 수정

--- backend/src/main/java/codesquad/team4/issuetracker/comment/dto/CommentResponseDto.java
+++ backend/src/main/java/codesquad/team4/issuetracker/comment/dto/CommentResponseDto.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import software.amazon.awssdk.annotations.NotNull;
 
 public class CommentResponseDto {
 
