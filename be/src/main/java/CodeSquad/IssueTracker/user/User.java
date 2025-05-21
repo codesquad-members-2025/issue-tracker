a2292197f0a29d676fe9f9fe3140dbd0ee2a57ba
jang-jinuk <dlsndifn98@gmail.com>
2025-05-21 16:12:04 +0900

    ✅ User 엔티티에 Lombok 생성자 및 빌더 추가
    - @NoArgsConstructor, @AllArgsConstructor, @Builder 애너테이션 추가

--- be/src/main/java/CodeSquad/IssueTracker/user/User.java
+++ be/src/main/java/CodeSquad/IssueTracker/user/User.java
@@ -1,10 +1,16 @@
 package CodeSquad.IssueTracker.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table("users")
 public class User {
     @Id
