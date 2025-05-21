99787a27649e13a8ddd8b20c733e67a1e416c5f4
doopang24 <wodus4145@naver.com>
2025-05-21 17:00:24 +0900

    refactor: repository 어노테이션 추가

--- backend/src/main/java/codesquad/team01/issuetracker/user/repository/UserRepository.java
+++ backend/src/main/java/codesquad/team01/issuetracker/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package codesquad.team01.issuetracker.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import codesquad.team01.issuetracker.user.domain.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 	Optional<User> findByLoginId(Integer id);
 
