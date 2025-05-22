2d702dcfc961afc98cb85865d47ee8b86ad29482
jang-jinuk <dlsndifn98@gmail.com>
2025-05-22 20:32:29 +0900

    ✨ 필수 사용자 정보만 가져오도록 변경
    - userService의 findAll() 대신 findUserDetails()를 사용하여 필요한 정보가 담긴 사용자 목록을 조회하도록 변경

--- be/src/main/java/CodeSquad/IssueTracker/home/dto/HomeResponseDto.java
+++ be/src/main/java/CodeSquad/IssueTracker/home/dto/HomeResponseDto.java
@@ -3,7 +3,7 @@ package CodeSquad.IssueTracker.home.dto;
 import CodeSquad.IssueTracker.issue.dto.FilteredIssueDto;
 import CodeSquad.IssueTracker.label.Label;
 import CodeSquad.IssueTracker.milestone.Milestone;
-import CodeSquad.IssueTracker.user.User;
+import CodeSquad.IssueTracker.user.dto.DetailUserDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,7 +11,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class HomeResponseDto {
     private Iterable<FilteredIssueDto> issues;
+    private Iterable<DetailUserDto> users;
     private Iterable<Label> labels;
     private Iterable<Milestone> milestones;
-    private Iterable<User> users;
 }
