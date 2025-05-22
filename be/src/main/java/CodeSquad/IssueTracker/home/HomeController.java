aeff9bfae352d2d072b3c16e68081235aabb2c1b
jang-jinuk <dlsndifn98@gmail.com>
2025-05-22 17:58:51 +0900

    ♻️ 필터 기반 이슈 응답을 위한 요청 필터 파라미터 처리 추가
    - 기존 HomeResponseDto 직접 반환 방식에서 BaseResponseDto 래핑 방식으로 변경
    - IssueFilterRequestDto를 @ModelAttribute로 받아 필터 조건 전달 가능하도록 구현
    - 홈 페이지에서도 필터링된 이슈 목록을 응답할 수 있도록 구조 개선
    
    (cherry picked from commit 41a8b9c29c54fb0119aeb17e2c9bc1af42512af5)

--- be/src/main/java/CodeSquad/IssueTracker/home/HomeController.java
+++ be/src/main/java/CodeSquad/IssueTracker/home/HomeController.java
@@ -1,29 +1,20 @@
 package CodeSquad.IssueTracker.home;
 
+import CodeSquad.IssueTracker.global.dto.BaseResponseDto;
+import CodeSquad.IssueTracker.home.dto.IssueFilterRequestDto;
 import CodeSquad.IssueTracker.home.dto.HomeResponseDto;
-import CodeSquad.IssueTracker.issue.Issue;
-import CodeSquad.IssueTracker.issue.IssueService;
-import CodeSquad.IssueTracker.label.LabelService;
-import CodeSquad.IssueTracker.milestone.MilestoneService;
-import CodeSquad.IssueTracker.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
 public class HomeController {
 
     private final HomeService homeService;
 
     @GetMapping("/")
-    public HomeResponseDto home() {
-        return homeService.getHomeData();
+    public BaseResponseDto<HomeResponseDto> home(@ModelAttribute IssueFilterRequestDto filterRequestDto) {
+        return BaseResponseDto.success("이슈목록을 성공적으로 불러왔습니다.",
+                homeService.getHomeData(filterRequestDto));
     }
-
-
 }
