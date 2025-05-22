8fa20d34cd78ed65f2152970e3ff0b27fed8c66e
jang-jinuk <dlsndifn98@gmail.com>
2025-05-22 16:12:47 +0900

    ✅ 이슈 필터링용 요약 DTO(Label, Milestone, User) 추가
    - 이슈 목록에서 보여줘야 할 필수 정보만 전달하도록 DTO(SummaryLabelDto, SummaryMilestoneDto, SummaryUserDto) 추가
    
    (cherry picked from commit 98c77b5a72e62ad9ee8886c54699752127aafdc3)

---
+++ be/src/main/java/CodeSquad/IssueTracker/user/dto/SummaryUserDto.java
@@ -0,0 +1,15 @@
+package CodeSquad.IssueTracker.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SummaryUserDto {
+    private Long id;
+    private String nickName;
+}
