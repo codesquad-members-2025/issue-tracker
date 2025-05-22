0ed1624bec8bae6275f9b904f9df5ddbbfd9b63f
jang-jinuk <dlsndifn98@gmail.com>
2025-05-22 17:56:32 +0900

    ♻️ HomeResponseDto의 이슈 타입을 FilteredIssueDto로 변경
    - 필터링된 이슈 정보를 응답할 수 있도록 Issue → FilteredIssueDto로 변경
    
    (cherry picked from commit 6cf97066bfba1420d13c8fc3084d2343ad77f8f1)

--- be/src/main/java/CodeSquad/IssueTracker/home/dto/HomeResponseDto.java
+++ be/src/main/java/CodeSquad/IssueTracker/home/dto/HomeResponseDto.java
@@ -1,18 +1,16 @@
 package CodeSquad.IssueTracker.home.dto;
 
-import CodeSquad.IssueTracker.issue.Issue;
+import CodeSquad.IssueTracker.issue.dto.FilteredIssueDto;
 import CodeSquad.IssueTracker.label.Label;
 import CodeSquad.IssueTracker.milestone.Milestone;
 import CodeSquad.IssueTracker.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 public class HomeResponseDto {
-    private Iterable<Issue> issues;
+    private Iterable<FilteredIssueDto> issues;
     private Iterable<Label> labels;
     private Iterable<Milestone> milestones;
     private Iterable<User> users;
