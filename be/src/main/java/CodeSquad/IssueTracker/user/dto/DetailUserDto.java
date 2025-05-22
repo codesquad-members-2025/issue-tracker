ebd589f38b3a15af2d03677fa4a2507cfbaad93a
jang-jinuk <dlsndifn98@gmail.com>
2025-05-22 20:24:20 +0900

    ✅ 상세한 사용자 정보 DTO 추가
    - id, nickname, imgUrl을 정보를 전달하는 DTO 추가

---
+++ be/src/main/java/CodeSquad/IssueTracker/user/dto/DetailUserDto.java
@@ -0,0 +1,16 @@
+package CodeSquad.IssueTracker.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailUserDto {
+    private Long id;
+    private String nickname;
+    private String imgUrl;
+}
