ec8720a5c7f7b9eddec0ef84abc1c8ac1a70327d
jkchoi490 <jkchoi490@daum.net>
2025-05-23 12:58:21 +0900

    feat: RefreshTokenRepository findByUserId 메서드 구현

--- backend/src/main/java/codesquad/team01/issuetracker/auth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/repository/RefreshTokenRepository.java
@@ -1,5 +1,7 @@
 package codesquad.team01.issuetracker.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import codesquad.team01.issuetracker.auth.domain.RefreshToken;
 
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+
+	Optional<RefreshToken> findByUserId(Integer userId);
 }
