1339718665ed9548c292441445724affec5705ad
jkchoi490 <jkchoi490@daum.net>
2025-05-23 12:53:01 +0900

    feat : RefreshToken 엔티티 생성

--- backend/src/main/java/codesquad/team01/issuetracker/auth/domain/RefreshToken.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/domain/RefreshToken.java
@@ -4,16 +4,19 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-//todo: DDL 작성, loginId -> id로
-//todo: token을 안전하게 저장해야함(ex. 암호화 등)
 @Getter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Table("refresh_tokens")
 public class RefreshToken {
 	@Id
 	private Integer id;
 	private Integer userId;
-	private String token; //암호화 해서 db에 저장해야 함!
+	private String token;
+
 }
