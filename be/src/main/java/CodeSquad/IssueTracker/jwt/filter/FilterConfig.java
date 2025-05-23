8cf6629c1daa05cfb14effbe78723f16247b2fd2
dongchan0105 <come753zz@gmail.com>
2025-05-24 02:10:10 +0900

    WebMvcConfigurer와 CorsFilter는 중복 적용되면 CORS가 작동 안 함

--- be/src/main/java/CodeSquad/IssueTracker/jwt/filter/FilterConfig.java
+++ be/src/main/java/CodeSquad/IssueTracker/jwt/filter/FilterConfig.java
@@ -3,24 +3,14 @@ package CodeSquad.IssueTracker.jwt.filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class FilterConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
 
-
     public FilterConfig(JwtAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
-    }
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(0);
-        return registrationBean;
     }
 
     @Bean
