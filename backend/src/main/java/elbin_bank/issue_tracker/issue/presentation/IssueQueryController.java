a1696f227c42697908b235cb06cfa6205fbf6498
HeeSung Choi <gladhee@icloud.com>
2025-05-26 00:31:43 +0900

    feat(IssueQueryController): add endpoint to retrieve issue details by ID

--- backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/IssueQueryController.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/IssueQueryController.java
@@ -1,13 +1,11 @@
 package elbin_bank.issue_tracker.issue.presentation;
 
 import elbin_bank.issue_tracker.issue.application.query.IssueQueryService;
+import elbin_bank.issue_tracker.issue.application.query.dto.IssueDetailResponseDto;
 import elbin_bank.issue_tracker.issue.application.query.dto.IssuesResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/issues")
@@ -17,8 +15,13 @@ public class IssueQueryController {
     private final IssueQueryService issueQueryService;
 
     @GetMapping("")
-    public ResponseEntity<IssuesResponseDto> list(@RequestParam(value = "q", required = false) String q) {
-        return ResponseEntity.ok(issueQueryService.getFilteredIssues(q));
+    public ResponseEntity<IssuesResponseDto> list(@RequestParam(value = "q", required = false) String rawQuery) {
+        return ResponseEntity.ok(issueQueryService.getFilteredIssues(rawQuery));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<IssueDetailResponseDto> getIssue(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(issueQueryService.getIssue(id));
     }
 
 }
