1f4cecb0dda4e8690906ef186ca881cc411f9791
HeeSung Choi <gladhee@icloud.com>
2025-05-26 00:28:18 +0900

    feat(Application): set JVM UTC timezone

--- backend/src/main/java/elbin_bank/issue_tracker/IssueTrackerApplication.java
+++ backend/src/main/java/elbin_bank/issue_tracker/IssueTrackerApplication.java
@@ -3,11 +3,15 @@ package elbin_bank.issue_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class IssueTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IssueTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // 전체 JVM의 기본 타임존을 UTC로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(IssueTrackerApplication.class, args);
+    }
 
 }
