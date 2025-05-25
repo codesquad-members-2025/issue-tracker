ff9c52423e5900f88ee5cf0fbc3aedc1619d49b5
HeeSung Choi <gladhee@icloud.com>
2025-05-26 00:31:23 +0900

    feat(IssueDtoMapper): implement projection to dto mapper

---
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/application/query/mapper/IssueDtoMapper.java
@@ -0,0 +1,61 @@
+package elbin_bank.issue_tracker.issue.application.query.mapper;
+
+import elbin_bank.issue_tracker.issue.application.query.dto.IssueDetailResponseDto;
+import elbin_bank.issue_tracker.issue.application.query.dto.IssueDto;
+import elbin_bank.issue_tracker.issue.application.query.dto.IssuesResponseDto;
+import elbin_bank.issue_tracker.issue.application.query.dto.MilestoneDto;
+import elbin_bank.issue_tracker.issue.infrastructure.query.projection.UserInfoProjection;
+import elbin_bank.issue_tracker.issue.infrastructure.query.projection.IssueCountProjection;
+import elbin_bank.issue_tracker.issue.infrastructure.query.projection.IssueDetailProjection;
+import elbin_bank.issue_tracker.issue.infrastructure.query.projection.IssueProjection;
+import elbin_bank.issue_tracker.label.infrastructure.query.projection.LabelProjection;
+import elbin_bank.issue_tracker.milestone.application.ProgressRateCalculator;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class IssueDtoMapper {
+
+    public IssuesResponseDto toIssuesResponseDto(List<IssueProjection> issueProjections,
+                                                 Map<Long, List<LabelProjection>> labelProjections,
+                                                 Map<Long, List<String>> assigneeNames,
+                                                 IssueCountProjection issueCountProjection) {
+        return new IssuesResponseDto(issueProjections.stream()
+                .map(b -> new IssueDto(
+                        b.id(), b.author(), b.title(),
+                        labelProjections.getOrDefault(b.id(), List.of()),
+                        b.isClosed(), b.createdAt(), b.updatedAt(),
+                        assigneeNames.getOrDefault(b.id(), List.of()),
+                        b.milestone()
+                )).toList(),
+                issueCountProjection.openCount(),
+                issueCountProjection.closedCount());
+    }
+
+    public IssueDetailResponseDto toIssueDetailDto(IssueDetailProjection issueDetailProjection,
+                                                   List<LabelProjection> labelProjections,
+                                                   List<UserInfoProjection> userInfoProjections) {
+        int progressRate = ProgressRateCalculator.calculate(issueDetailProjection.milestoneTotalIssues(),
+                issueDetailProjection.milestoneClosedIssues());
+
+        return new IssueDetailResponseDto(
+                issueDetailProjection.id(),
+                new UserInfoProjection(issueDetailProjection.authorId(),
+                        issueDetailProjection.authorNickname(),
+                        issueDetailProjection.authorProfileImage()),
+                issueDetailProjection.title(),
+                issueDetailProjection.contents(),
+                labelProjections,
+                new MilestoneDto(issueDetailProjection.milestoneId(),
+                        issueDetailProjection.milestoneName(),
+                        progressRate),
+                userInfoProjections,
+                issueDetailProjection.isClosed(),
+                issueDetailProjection.createdAt(),
+                issueDetailProjection.updatedAt()
+        );
+    }
+
+}
