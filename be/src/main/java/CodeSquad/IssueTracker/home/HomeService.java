8ee0ea0c959b705dd37a485699d44ffd224a5976
jang-jinuk <dlsndifn98@gmail.com>
2025-05-26 20:13:47 +0900

    ✨  홈 데이터 응답에 메타데이터(페이지 정보 및 이슈 상태 수) 추가
    - HomeResponseDto에 MetaData 추가
    - 현재 페이지, 전체 페이지 수, 열린 이슈 수, 닫힌 이슈 수 포함
    - 이슈 목록 페이징 처리 지원

--- be/src/main/java/CodeSquad/IssueTracker/home/HomeService.java
+++ be/src/main/java/CodeSquad/IssueTracker/home/HomeService.java
@@ -2,6 +2,7 @@ package CodeSquad.IssueTracker.home;
 
 import CodeSquad.IssueTracker.home.dto.IssueFilterRequestDto;
 import CodeSquad.IssueTracker.home.dto.HomeResponseDto;
+import CodeSquad.IssueTracker.home.dto.MetaData;
 import CodeSquad.IssueTracker.issue.IssueService;
 import CodeSquad.IssueTracker.label.LabelService;
 import CodeSquad.IssueTracker.milestone.MilestoneService;
@@ -18,12 +19,16 @@ public class HomeService {
     private final MilestoneService milestoneService;
     private final UserService userService;
 
-    public HomeResponseDto getHomeData(IssueFilterRequestDto filterRequestDto) {
+    public HomeResponseDto getHomeData(int currentPage, IssueFilterRequestDto filterRequestDto) {
+        int openIssueNumber = issueService.countIssuesByOpenStatus(true, filterRequestDto);
+        int closeIssueNumber = issueService.countIssuesByOpenStatus(false, filterRequestDto);
+        int maxPage = issueService.getIssueMaxPage(filterRequestDto);
         return new HomeResponseDto(
-                issueService.findIssuesByFilter(filterRequestDto),
+                issueService.findIssuesByFilter(currentPage, filterRequestDto),
                 userService.findAllUserDetails(),
                 labelService.findAll(),
-                milestoneService.findAll()
+                milestoneService.findAll(),
+                new MetaData(currentPage, maxPage, openIssueNumber, closeIssueNumber)
         );
     }
 }
