076031184992d04a74e15fa380b90f2abc8e7b5d
doopang24 <wodus4145@naver.com>
2025-05-27 09:53:11 +0900

    chore: 불필요한 어노테이션 제거

--- backend/src/main/java/codesquad/team01/issuetracker/label/repository/LabelRepository.java
+++ backend/src/main/java/codesquad/team01/issuetracker/label/repository/LabelRepository.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import codesquad.team01.issuetracker.label.domain.Label;
 import codesquad.team01.issuetracker.label.dto.LabelDto;
 
-@Repository
 public interface LabelRepository extends CrudRepository<Label, Integer> {
 	@Query("""
 		SELECT 
