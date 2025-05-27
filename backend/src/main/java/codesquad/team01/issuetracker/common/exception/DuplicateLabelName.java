4e3fbd3b40d341cd18b1cb9fb2ed92547f7c3367
doopang24 <wodus4145@naver.com>
2025-05-27 20:22:58 +0900

    feat: 이름이 중복된 레이블이 있을 때 발생하는 커스텀 exception

---
+++ backend/src/main/java/codesquad/team01/issuetracker/common/exception/DuplicateLabelName.java
@@ -0,0 +1,7 @@
+package codesquad.team01.issuetracker.common.exception;
+
+public class DuplicateLabelName extends RuntimeException {
+	public DuplicateLabelName(String message) {
+		super(message);
+	}
+}
