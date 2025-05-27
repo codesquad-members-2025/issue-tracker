f2d5c9ba4582f2c5b76bb47826a1e6748cfb16ba
HeeSung Choi <gladhee@icloud.com>
2025-05-27 22:30:19 +0900

    feat(UserQueryController): add controller for retrieving user information

---
+++ backend/src/main/java/elbin_bank/issue_tracker/user/presentation/UserQueryController.java
@@ -0,0 +1,23 @@
+package elbin_bank.issue_tracker.user.presentation;
+
+import elbin_bank.issue_tracker.user.application.query.UserQueryService;
+import elbin_bank.issue_tracker.user.application.query.dto.UserInfoResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/users")
+@RequiredArgsConstructor
+public class UserQueryController {
+
+    private final UserQueryService userQueryService;
+
+    @GetMapping("")
+    public ResponseEntity<UserInfoResponseDto> getUsers() {
+        return ResponseEntity.ok(userQueryService.findUsers());
+    }
+
+}
