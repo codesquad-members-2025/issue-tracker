b51c2dc22ab33b57453e3ced1fecaeaf266ce27c
youngmin <dudals2963@tukrorea.ac.kr>
2025-05-27 14:15:55 +0900

    ♻️ UseQueryParams함수 개선
    여러개의 변경값들도 한번에 처리할 수 있도록 개선 완료.

--- fe/src/utils/queryParams/useQueryParams.js
+++ fe/src/utils/queryParams/useQueryParams.js
@@ -2,13 +2,15 @@ import { useSearchParams } from 'react-router-dom';
 
 export default function useQueryParams() {
   const [searchParams, setSearchParams] = useSearchParams();
-  function editParams(key, value) {
+  function editParams(newParams) {
     const paramsObject = Object.fromEntries(Array.from(searchParams.entries()));
-    if (value === undefined || value === null) {
-      delete paramsObject[key];
-    } else {
-      paramsObject[key] = value;
-    }
+    Object.entries(newParams).forEach(([key, value]) => {
+      if (value === undefined || value === null) {
+        delete paramsObject[key];
+      } else {
+        paramsObject[key] = value;
+      }
+    });
     setSearchParams(paramsObject);
   }
 
