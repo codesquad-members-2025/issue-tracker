3638a3b84d8994812313592af6ba2060d5545707
HeeSung Choi <gladhee@icloud.com>
2025-05-27 21:15:44 +0900

    feat(IssueCommandService): implement issue creation logic and response DTO

---
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/application/command/IssueCommandService.java
@@ -0,0 +1,37 @@
+package elbin_bank.issue_tracker.issue.application.command;
+
+import elbin_bank.issue_tracker.issue.application.command.dto.IssueCreateResponseDto;
+import elbin_bank.issue_tracker.issue.domain.Issue;
+import elbin_bank.issue_tracker.issue.domain.IssueCommandRepository;
+import elbin_bank.issue_tracker.issue.presentation.command.dto.request.IssueCreateRequestDto;
+import elbin_bank.issue_tracker.label.domain.LabelCommandRepository;
+import elbin_bank.issue_tracker.user.domain.UserCommandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class IssueCommandService {
+
+    private final LabelCommandRepository labelCommandRepository;
+    private final IssueCommandRepository issueCommandRepository;
+    private final UserCommandRepository userCommandRepository;
+
+    @Transactional
+    public IssueCreateResponseDto createIssue(IssueCreateRequestDto requestDto) {
+        Issue issue = Issue.of(
+                1L, // todo jwt에서 꺼내와야댐
+                requestDto.title(),
+                requestDto.content(),
+                requestDto.milestone(),
+                false
+        );
+        Issue newIssue = issueCommandRepository.save(issue);
+        labelCommandRepository.saveLabelsToIssue(newIssue.getId(), requestDto.labels());
+        userCommandRepository.saveAssigneesToIssue(newIssue.getId(), requestDto.assignees());
+
+        return new IssueCreateResponseDto(newIssue.getId());
+    }
+
+}
---
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/application/command/dto/IssueCreateResponseDto.java
@@ -0,0 +1,4 @@
+package elbin_bank.issue_tracker.issue.application.command.dto;
+
+public record IssueCreateResponseDto(long id) {
+}
