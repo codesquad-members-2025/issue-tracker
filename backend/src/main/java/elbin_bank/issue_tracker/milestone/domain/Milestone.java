7bdcc92f0ecad0aacdb5ed4bcc73c4c07493f483
HeeSung Choi <gladhee@icloud.com>
2025-05-28 19:59:32 +0900

    refactor(Milestone): remove unused import statements

--- backend/src/main/java/elbin_bank/issue_tracker/milestone/domain/Milestone.java
+++ backend/src/main/java/elbin_bank/issue_tracker/milestone/domain/Milestone.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
@@ -26,6 +25,3 @@ public class Milestone extends BaseEntity {
     private LocalDate expiredAt;
 
 }
-
-
-
