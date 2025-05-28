f321d940ecc2dd386b26360036a12c19800b9b93
jkchoi490 <jkchoi490@daum.net>
2025-05-28 09:12:16 +0900

    resolve : conflict 해결

--- backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/api/AuthController.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +19,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
--- backend/src/main/java/codesquad/team01/issuetracker/auth/service/TokenService.java
+++ backend/src/main/java/codesquad/team01/issuetracker/auth/service/TokenService.java
@@ -57,8 +57,6 @@ public class TokenService {
 				.build();
 			refreshTokenRepository.save(created);
 		}
-
 		return new AuthDto.LoginResponse(accessToken, refreshToken);
 	}
-
 }
