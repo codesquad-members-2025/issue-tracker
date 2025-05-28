820e215e9db227509fda25ef386274c008e6220d
HeeSung Choi <gladhee@icloud.com>
2025-05-28 20:25:37 +0900

    feat(MilestoneQueryController): return ResponseEntity for milestone queries

--- backend/src/main/java/elbin_bank/issue_tracker/milestone/presentation/query/MilestoneQueryController.java
+++ backend/src/main/java/elbin_bank/issue_tracker/milestone/presentation/query/MilestoneQueryController.java
@@ -4,11 +4,10 @@ import elbin_bank.issue_tracker.milestone.application.query.MilestoneQueryServic
 import elbin_bank.issue_tracker.milestone.application.query.dto.MilestoneShortsResponseDto;
 import elbin_bank.issue_tracker.milestone.application.query.dto.MilestonesResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/milestones")
@@ -18,13 +17,13 @@ public class MilestoneQueryController {
     private final MilestoneQueryService milestoneQueryService;
 
     @GetMapping("")
-    public MilestonesResponseDto getMilestones() {
-        return milestoneQueryService.getMilestones();
+    public ResponseEntity<MilestonesResponseDto> getMilestones() {
+        return ResponseEntity.ok(milestoneQueryService.getMilestones());
     }
 
     @GetMapping("/short")
-    public MilestoneShortsResponseDto getMilestonesForSelectBox() {
-        return milestoneQueryService.getMilestonesForSelectBox();
+    public ResponseEntity<MilestoneShortsResponseDto> getMilestonesForSelectBox() {
+        return ResponseEntity.ok(milestoneQueryService.getMilestonesForSelectBox());
     }
 
 }
