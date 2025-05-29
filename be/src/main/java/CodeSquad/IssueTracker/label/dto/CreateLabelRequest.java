1267954ebf501cf1699531cdd44e276348d1ede8
dongchan0105 <come753zz@gmail.com>
2025-05-30 00:38:53 +0900

    ✅  테스트 브랜치에서도 deploy 테스트 할 수 있도록 설정

--- be/src/main/java/CodeSquad/IssueTracker/label/dto/CreateLabelRequest.java
+++ be/src/main/java/CodeSquad/IssueTracker/label/dto/CreateLabelRequest.java
@@ -1,12 +1,14 @@
 package CodeSquad.IssueTracker.label.dto;
 
 import CodeSquad.IssueTracker.label.Label;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class CreateLabelRequest {
+    @NotBlank(message = "라벨의 이름은 필수로 들어가야합니다")
     private String name;
     private String description;
     private String color;
