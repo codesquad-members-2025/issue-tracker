19e80a951d47b4d87476f9b494199e8b49bfee5f
doopang24 <wodus4145@naver.com>
2025-05-29 21:55:16 +0900

    feat: 마일스톤 엔티티 구현

--- backend/src/main/java/codesquad/team01/issuetracker/milestone/domain/Milestone.java
+++ backend/src/main/java/codesquad/team01/issuetracker/milestone/domain/Milestone.java
@@ -1,4 +1,37 @@
 package codesquad.team01.issuetracker.milestone.domain;
 
-public class Milestone {
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import codesquad.team01.issuetracker.common.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table("milestone")
+public class Milestone extends BaseEntity {
+
+	@Id
+	private Integer id;
+
+	private String title;
+
+	private String description;
+
+	private LocalDate dueDate;
+
+	private MilestoneState state;
+
+	@Builder
+	private Milestone(String title, String description, LocalDate dueDate, MilestoneState state) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.state = state;
+	}
 }
