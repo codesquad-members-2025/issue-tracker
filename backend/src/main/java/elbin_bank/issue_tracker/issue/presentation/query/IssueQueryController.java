aef00ba8fff0a4474dcd43b7a3662efe168d7d13
HeeSung Choi <gladhee@icloud.com>
2025-05-29 14:32:34 +0900

    feat(IssueQueryController): add endpoints to retrieve labels, assignees, and milestone for an issue

--- backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/query/IssueQueryController.java
+++ backend/src/main/java/elbin_bank/issue_tracker/issue/presentation/query/IssueQueryController.java
@@ -1,8 +1,7 @@
 package elbin_bank.issue_tracker.issue.presentation.query;
 
 import elbin_bank.issue_tracker.issue.application.query.IssueQueryService;
-import elbin_bank.issue_tracker.issue.application.query.dto.IssueDetailResponseDto;
-import elbin_bank.issue_tracker.issue.application.query.dto.IssuesResponseDto;
+import elbin_bank.issue_tracker.issue.application.query.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +19,23 @@ public class IssueQueryController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<IssueDetailResponseDto> getIssue(@PathVariable("id") Integer id) {
+    public ResponseEntity<IssueDetailResponseDto> getIssue(@PathVariable("id") long id) {
         return ResponseEntity.ok(issueQueryService.getIssue(id));
+    }
+
+    @GetMapping("{id}/labels")
+    public ResponseEntity<LabelsResponseDto> getLabelsForIssue(@PathVariable("id") long id) {
+        return ResponseEntity.ok(issueQueryService.getLabelsRelatedToIssue(id));
+    }
+
+    @GetMapping("{id}/assignees")
+    public ResponseEntity<AssigneesResponseDto> getAssigneesForIssue(@PathVariable("id") long id) {
+        return ResponseEntity.ok(issueQueryService.getAssigneesRelatedToIssue(id));
+    }
+
+    @GetMapping("{id}/milestone")
+    public ResponseEntity<MilestoneResponseDto> getMilestoneForIssue(@PathVariable("id") long id) {
+        return ResponseEntity.ok(issueQueryService.getMilestoneForIssue(id));
     }
 
 }
