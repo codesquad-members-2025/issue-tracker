47eafa706c964cb82966a367a0f6a70282af9539
Gyuwon Moon <gyuwon822@gmail.com>
2025-05-29 17:44:48 +0900

    feat: access token으로 GitHub 사용자 정보 요청 - fetchGitHubUser(accessToken) 메서드 구현 - Authorization: Bearer 헤더 포함 GET 요청 - GitHubUserResponse DTO로 응답 파싱

--- backend/src/main/java/codesquad/team4/issuetracker/oauth/OAuthService.java
+++ backend/src/main/java/codesquad/team4/issuetracker/oauth/OAuthService.java
@@ -1,12 +1,18 @@
 package codesquad.team4.issuetracker.oauth;
 
+import codesquad.team4.issuetracker.exception.badrequest.InvalidOAuthStateException;
+import codesquad.team4.issuetracker.oauth.dto.OAuthRequestDto;
 import codesquad.team4.issuetracker.oauth.dto.OAuthResponseDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -18,9 +24,17 @@ public class OAuthService {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
-    private static final String GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
+    @Value("${github.client.secret}")
+    private String clientSecret;
 
-    public OAuthResponseDto.OAuthLoginUrlResponse createGithubAuthorizeUrl(HttpSession session) {
+    private final RestTemplate restTemplate;
+
+    private static final String GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
+    private static final String GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token";
+    private static final String GITHUB_USER_API_URL = "https://api.github.com/user";
+
+
+    public OAuthResponseDto.OAuthLoginUrl createGithubAuthorizeUrl(HttpSession session) {
         //state 생성 및 세션에 저장
         String state = UUID.randomUUID().toString();
         session.setAttribute("oauth_state", state);
@@ -34,8 +48,67 @@ public class OAuthService {
             .build()
             .toUriString();
 
-        return OAuthResponseDto.OAuthLoginUrlResponse.builder()
+        return OAuthResponseDto.OAuthLoginUrl.builder()
             .url(githubUrl)
             .build();
+    }
+
+    public void handleCallback(OAuthRequestDto.GitHubCallback callback, HttpSession session) {
+        //state 값 비교
+        String savedState = (String) session.getAttribute("oauth_state");
+        if (!callback.getState().equals(savedState)) {
+            throw new InvalidOAuthStateException();
+        }
+
+        //access tocken 요청
+        String accessToken = requestAccessToken(callback.getCode());
+        //Github 사용자 정보 요청
+        OAuthResponseDto.GitHubUserResponse userInfo = fetchGitHubUser(accessToken);
+        //(아직 구현 안함)사용자 DB 확인 -> 회원가입/로그인 처리
+    }
+
+    private String requestAccessToken(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        //request
+        Map<String, String> body = Map.of(
+            "client_id", clientId,
+            "client_secret", clientSecret,
+            "code", code,
+            "redirect_uri", redirectUri
+        );
+
+        //header, body를 합쳐서 하나의 요청으로 만듦
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+
+        //Post 요청
+        ResponseEntity<Map> response = restTemplate.postForEntity(
+            GITHUB_TOKEN_URL,
+            request,
+            Map.class
+        );
+
+        return (String) response.getBody().get("access_token");
+    }
+
+    private OAuthResponseDto.GitHubUserResponse fetchGitHubUser(String accessToken) {
+        //Authorization 포함된 헤더
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        //Get 요청
+        ResponseEntity<OAuthResponseDto.GitHubUserResponse> response = restTemplate.exchange(
+            GITHUB_USER_API_URL,
+            HttpMethod.GET,
+            request,
+            OAuthResponseDto.GitHubUserResponse.class
+        );
+
+        return response.getBody();
     }
 }
