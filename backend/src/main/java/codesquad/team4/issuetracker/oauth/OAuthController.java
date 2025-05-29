89e572ea273db4b127ae9cdf91d84a90737522c9
Gyuwon Moon <gyuwon822@gmail.com>
2025-05-29 17:42:43 +0900

    feat: GitHub OAuth 콜백 컨트롤러 추가 - /api/auth/callback 엔드포인트 구현 - code, state 파라미터 수신 - OAuthService의 handleCallback 위임 처리

--- backend/src/main/java/codesquad/team4/issuetracker/oauth/OAuthController.java
+++ backend/src/main/java/codesquad/team4/issuetracker/oauth/OAuthController.java
@@ -1,11 +1,14 @@
 package codesquad.team4.issuetracker.oauth;
 
+import codesquad.team4.issuetracker.oauth.dto.OAuthRequestDto;
 import codesquad.team4.issuetracker.oauth.dto.OAuthResponseDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +18,14 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/login")
-    public ResponseEntity<String> githubLogin(HttpSession session) {
-        OAuthResponseDto.OAuthLoginUrlResponse response = oAuthService.createGithubAuthorizeUrl(session);
+    public ResponseEntity<OAuthResponseDto.OAuthLoginUrl> githubLogin(HttpSession session) {
+        OAuthResponseDto.OAuthLoginUrl response = oAuthService.createGithubAuthorizeUrl(session);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/callback")
+    public ResponseEntity<String> callback( @RequestParam String code, @RequestParam String state, HttpSession session) {
+        oAuthService.handleCallback(new OAuthRequestDto.GitHubCallback(code, state), session);
+        return ResponseEntity.ok("인증성공"); //여기 바꿔줘야 함.... 홈화면?.., 몰라 그냥 상태만?
     }
 }
