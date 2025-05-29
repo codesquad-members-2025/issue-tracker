ee39b3b5d71887f2ecc18716cabf01d90299e692
jang-jinuk <dlsndifn98@gmail.com>
2025-05-29 11:55:22 +0900

    ✅ GitHub OAuth 액세스 토큰 요청 기능 구현
    - GithubApiClient 클래스 생성
    - WebClient를 사용해 GitHub OAuth 서버에 access token 요청

---
+++ be/src/main/java/CodeSquad/IssueTracker/oauth/GithubApiClient.java
@@ -0,0 +1,57 @@
+package CodeSquad.IssueTracker.oauth;
+
+import CodeSquad.IssueTracker.oauth.dto.GithubAccessTokenRequest;
+import CodeSquad.IssueTracker.oauth.dto.GithubAccessTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Objects;
+
+
+@Component
+@RequiredArgsConstructor
+public class GithubApiClient {
+
+    private final WebClient webClient;
+
+    @Value("${spring.github.client-id}")
+    private String clientId;
+
+    @Value("${spring.github.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.github.redirect-uri}")
+    private String redirectUri;
+
+    /*
+     * GitHub OAuth 요청에 필요한 클라이언트 ID, 시크릿, 리다이렉트 URI를 포함해
+     * 인가 코드를 사용하여 Access Token을 받아옵니다.
+     */
+    public String getAccessToken(String code) {
+        String url = "https://github.com/login/oauth/access_token";
+
+        GithubAccessTokenRequest accessTokenRequest = GithubAccessTokenRequest.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .redirect_uri(redirectUri)
+                .code(code)
+                .build();
+
+        GithubAccessTokenResponse githubAccessToken = webClient.post()
+                .uri(url)
+                .headers(headers -> {
+                    headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                })
+                .bodyValue(accessTokenRequest)
+                .retrieve()
+                .bodyToMono(GithubAccessTokenResponse.class)
+                .block(); // 결과를 기다려서 동기적으로 반환
+
+        return Objects.requireNonNull(githubAccessToken).getAccessToken();
+    }
+}
