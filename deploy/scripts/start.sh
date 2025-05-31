d09d9a5603c29911f541d90dbafee60d019d70c9
dongchan0105 <come753zz@gmail.com>
2025-05-31 13:46:09 +0900

    환경변수 주입

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-echo "> Start script running..."
+echo "> [START] 실행 시작"
 
-# .env 파일 생성 (GitHub Secrets에서 전달된 값을 직접 여기서 설정)
-cat <<EOF > /home/ubuntu/app/.env
+cd /home/ubuntu/app
+
+# 기존 실행 중인 프로세스 종료
+echo "> 기존 백엔드 종료 시도"
+pkill -f 'java -jar' || true
+
+# .env 파일 생성
+echo "> 환경변수 파일 생성"
+cat <<EOF > .env
 JWT_ACCESS_KEY=${JWT_ACCESS_KEY}
 JWT_REFRESH_KEY=${JWT_REFRESH_KEY}
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 AWS_REGION=${AWS_REGION}
 cloud.aws.region.static=${AWS_REGION}
-cloud.aws.s3.bucket=${S3_BUCKET}
+cloud.aws.s3.bucket=${S3_BUCKET_NAME}
 EOF
 
-source /home/ubuntu/app/.env
+# .env 적용
+echo "> 환경변수 적용"
+source .env
 
-echo "> Kill previous app..."
-pkill -f 'java -jar' || true
-
-echo "> Start app..."
+# 백엔드 애플리케이션 실행
+echo "> 백엔드 애플리케이션 실행"
 nohup java \
   -Dspring.profiles.active=dev \
   -DJWT_ACCESS_KEY=$JWT_ACCESS_KEY \
@@ -25,5 +32,5 @@ nohup java \
   -DAWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -DAWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -Dcloud.aws.region.static=$AWS_REGION \
-  -Dcloud.aws.s3.bucket=$S3_BUCKET \
+  -Dcloud.aws.s3.bucket=$S3_BUCKET_NAME \
   -jar /home/ubuntu/app/app.jar > /home/ubuntu/app/app.log 2>&1 &
--- deploy/scripts/stop.sh
+++ deploy/scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "> Stop previous app..."
+echo "> [STOP] 실행 중인 애플리케이션 종료"
+
 pkill -f 'java -jar' || true
