6bfb899be35b6c28ae1ff6eaead8525e14e07a6e
jang-jinuk <dlsndifn98@gmail.com>
2025-05-31 17:01:26 +0900

    🧪 이슈 상태 일괄 변경 기능 테스트 작성
    - issueService의 updateIssueOpenState가 issueRepository의 updateIsOpen을 잘 실행하는지 테스트 함
    - 테스트를 위한 @AllArgsConstructor 추가

--- be/src/main/java/CodeSquad/IssueTracker/issue/dto/IssueStatusUpdateRequest.java
+++ be/src/main/java/CodeSquad/IssueTracker/issue/dto/IssueStatusUpdateRequest.java
@@ -1,11 +1,13 @@
 package CodeSquad.IssueTracker.issue.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class IssueStatusUpdateRequest {
     @JsonProperty("id")
     private List<Long> issueIds;
