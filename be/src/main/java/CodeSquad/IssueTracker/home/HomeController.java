da5f1b0fd9fa9a451209449b0fe5d10e5dfd330d
dongchan0105 <come753zz@gmail.com>
2025-06-02 00:36:01 +0900

    최종 CICD

--- be/src/main/java/CodeSquad/IssueTracker/home/HomeController.java
+++ be/src/main/java/CodeSquad/IssueTracker/home/HomeController.java
@@ -12,8 +12,8 @@ public class HomeController {
 
     private final HomeService homeService;
 
-    @GetMapping("/")
-    public BaseResponseDto<HomeResponseDto> home(@RequestParam int page, @ModelAttribute IssueFilterCondition condition) {
+    @GetMapping("/home")
+    public BaseResponseDto<HomeResponseDto> home(@RequestParam(defaultValue = "1") int page, @ModelAttribute IssueFilterCondition condition) {
         return BaseResponseDto.success("이슈목록을 성공적으로 불러왔습니다.",
                 homeService.getHomeData(page, condition));
     }
