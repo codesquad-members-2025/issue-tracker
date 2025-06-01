899a79cc7fe93c8683ca1380d92cdd585302be71
dongchan0105 <come753zz@gmail.com>
2025-06-01 21:24:22 +0900

    배포용 커밋

--- deploy/scripts/start.sh
+++ deploy/scripts/start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-echo "> [START] Spring Boot App 실행"
 
-# .env 복사 (처음 배포된 환경변수 템플릿을 실제로 적용)
-cp /home/ubuntu/app/application.env /home/ubuntu/.env
+cd /home/ubuntu/app
+source application.env
 
-# 환경변수 불러오기
-source /home/ubuntu/.env
-
-# 실행 중이면 종료
+# 기존 프로세스 종료
 PID=$(pgrep -f 'java -jar')
 if [ -n "$PID" ]; then
   kill -9 $PID
-  echo "> 이전 프로세스 종료됨: $PID"
+  echo "✅ 이전 프로세스 종료됨: $PID"
 fi
 
 # 새로 실행
@@ -24,7 +20,6 @@ nohup java \
   -DAWS_REGION=$AWS_REGION \
   -Dcloud.aws.region.static=$AWS_REGION \
   -Dcloud.aws.s3.bucket=$S3_BUCKET_NAME \
-  -Dspring.web.resources.static-locations=file:/home/ubuntu/app/frontend/ \
-  -jar /home/ubuntu/app/app.jar > /home/ubuntu/app.log 2>&1 &
-
-echo "> Spring Boot 실행 완료"
+  -Dcloud.aws.s3.url=$S3_URL_NAME \
+  -Dspring.datasource.password=$DB_PASSWORD \
+  -jar app.jar > /home/ubuntu/app.log 2>&1 &
