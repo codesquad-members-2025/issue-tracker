a24fdfa3c3d90f76289cf29aa15a6111f2ee2919
jkchoi490 <jkchoi490@daum.net>
2025-06-02 17:10:09 +0900

    feat: findByUsername, findByEmail 메서드 추가

--- backend/src/main/java/codesquad/team01/issuetracker/user/repository/UserRepository.java
+++ backend/src/main/java/codesquad/team01/issuetracker/user/repository/UserRepository.java
@@ -3,12 +3,15 @@ package codesquad.team01.issuetracker.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import codesquad.team01.issuetracker.user.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>, UserQueryRepository {
 	Optional<User> findByLoginId(String id);
+
+	Optional<User> findByUsername(String username);
+
+	Optional<User> findByEmail(String email);
 
 	Optional<User> findByProviderIdAndAuthProvider(Long providerId, String authProvider);
 
