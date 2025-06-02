0e523e4944063848e365950a745ca963a20f8992
doopang24 <wodus4145@naver.com>
2025-06-02 11:50:49 +0900

    feat: 마일스톤 관련 커스텀 예외 구현

--- backend/src/main/java/codesquad/team01/issuetracker/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/codesquad/team01/issuetracker/common/exception/GlobalExceptionHandler.java
@@ -51,20 +51,36 @@ public class GlobalExceptionHandler {
 			.body(ApiResponse.error("레이블 '" + id + "' 을(를) 찾을 수 없습니다."));
 	}
 
+	@ExceptionHandler(InvalidDateException.class)
+	public ResponseEntity<ApiResponse<?>> handleInvalidDate(InvalidDateException e) {
+		log.error("InvalidDateException: {}", e.getMessage());
+		return ResponseEntity
+			.badRequest()
+			.body(ApiResponse.error(e.getMessage()));
+	}
+
+	@ExceptionHandler(MilestoneNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> handleMilestoneNotFound(MilestoneNotFoundException e) {
+		int id = e.getId();
+		return ResponseEntity
+			.badRequest()
+			.body(ApiResponse.error("마일스톤 '" + id + "' 을(를) 찾을 수 없습니다."));
+	}
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiResponse<?>> handleExceptions(Exception e) {
 		log.error("Unhandled exception", e);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error("서버 내부 오류가 발생했습니다."));
 	}
 
-	//아이디가 존재하지 않는 경우
+	// 아이디가 존재하지 않는 경우
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<ApiResponse<?>> handleUserNotFound(UserNotFoundException e) {
 		log.error("UserNotFoundException : {}", e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error("존재하지 않는 ID 입니다"));
 	}
 
-	//패스워드가 틀린 경우
+	// 패스워드가 틀린 경우
 	@ExceptionHandler(InvalidPasswordException.class)
 	public ResponseEntity<ApiResponse<?>> handleInvalidPassword(InvalidPasswordException e) {
 		log.error("InvalidPasswordException : {}", e.getMessage());
