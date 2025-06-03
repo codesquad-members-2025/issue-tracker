e8308ea482474195311b59bacaa46353997ef1cb
jang-jinuk <dlsndifn98@gmail.com>
2025-06-03 17:19:02 +0900

    ✅ 레이블 삭제 API 엔드포인트 추가

--- be/src/main/java/CodeSquad/IssueTracker/global/message/SuccessMessage.java
+++ be/src/main/java/CodeSquad/IssueTracker/global/message/SuccessMessage.java
@@ -7,7 +7,8 @@ public enum SuccessMessage {
     GITHUB_LOGIN_SUCCESS("Github 로그인을 성공했습니다"),
     ISSUE_DETAIL_FETCH_SUCCESS("이슈 상세 정보를 성공적으로 조회했습니다."),
     COMMENT_DELETE_SUCCESS("코멘트 삭제가 정상적으로 처리되었습니다."),
-    ISSUE_DELETE_SUCCESS("이슈 삭제가 정상적으로 치리되었습니다.");
+    ISSUE_DELETE_SUCCESS("이슈 삭제가 정상적으로 치리되었습니다."),
+    LABEL_DELETE_SUCCESS("라벨 삭제가 정상적으로 처리되었습니다.");
 
     private final String message;
 
