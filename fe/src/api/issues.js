f3312c85dc361ed682ca787622d6ba46dc238b43
dongchan <come753zz@gmail.com>
2025-06-04 11:04:59 +0900

    [Refactor]  issues.js에 충돌마커 삭제

--- fe/src/api/issues.js
+++ fe/src/api/issues.js
@@ -2,7 +2,5 @@ import { BASE_URL } from '.';
 import { TEST_URL } from '.';
 
 export const ISSUES_URL = `${BASE_URL}/home`;
-<<<<<<< Updated upstream
-=======
 export const TEST_ISSUES_URL = `${TEST_URL}/`;
->>>>>>> Stashed changes
+
