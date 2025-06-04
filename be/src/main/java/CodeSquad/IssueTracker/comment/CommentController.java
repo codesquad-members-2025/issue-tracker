1cfeeb92240ee40761e182f434e4c23afc87bf89
jang-jinuk <dlsndifn98@gmail.com>
2025-06-04 20:39:16 +0900

    ✅ 코멘트 관련 API 반환 형식 일치화
    - 코멘트 관련 API의 반환형식을 BaseResponseDto를 통해 반환하도록 구현

--- be/src/main/java/CodeSquad/IssueTracker/comment/CommentController.java
+++ be/src/main/java/CodeSquad/IssueTracker/comment/CommentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-import static CodeSquad.IssueTracker.global.message.SuccessMessage.COMMENT_DELETE_SUCCESS;
+import static CodeSquad.IssueTracker.global.message.SuccessMessage.*;
 
 @RestController
 @RequestMapping("/issues/{issueId}/comments")
@@ -26,25 +26,27 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CommentResponseDto writeComment(@RequestPart("data") @Validated CommentRequestDto request,
+    public BaseResponseDto<CommentResponseDto> writeComment(@RequestPart("data") @Validated CommentRequestDto request,
                                            @RequestPart(value = "files", required = false) List<MultipartFile> files,
                                            HttpServletRequest httpRequest) throws IOException {
         String authorId = httpRequest.getAttribute("id").toString();
         log.info("Writing comment with author id {}", authorId);
-        return commentService.createComment(request,files, Long.valueOf(authorId));
+        return BaseResponseDto.success(COMMENT_LIST_FETCH_SUCCESS.getMessage(),
+                commentService.createComment(request, files, Long.valueOf(authorId)));
     }
 
     @PatchMapping("/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable("commentId") Long commentId,
+    public BaseResponseDto<CommentResponseDto> updateComment(@PathVariable("commentId") Long commentId,
                                             @RequestPart("data") CommentUpdateDto request,
                                             @RequestPart(value = "files", required = false) List<MultipartFile> files,
                                             HttpServletRequest httpRequest) throws IOException {
         String authorId = httpRequest.getAttribute("id").toString();
-        return commentService.update(commentId,request,files, Long.valueOf(authorId));
+        return BaseResponseDto.success(COMMENT_UPDATE_SUCCESS.getMessage(),
+                commentService.update(commentId, request, files, Long.valueOf(authorId)));
     }
 
     @DeleteMapping("/{commentId}")
-    public BaseResponseDto<String> deleteComment(@PathVariable("commentId") Long commentId) {
+    public BaseResponseDto deleteComment(@PathVariable("commentId") Long commentId) {
         commentService.deleteById(commentId);
         return BaseResponseDto.success(COMMENT_DELETE_SUCCESS.getMessage(), null);
     }
